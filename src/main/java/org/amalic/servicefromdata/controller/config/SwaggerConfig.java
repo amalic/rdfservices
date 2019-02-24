640122cae95d1c30b6fbc3c778d3cd7a3cbe9580
Alexander Malic <alexander.malic@gmail.com>
2019-02-24 22:26:20 +0100

    expanded service-categories by default

--- src/main/java/org/amalic/servicefromdata/controller/config/SwaggerConfig.java
+++ src/main/java/org/amalic/servicefromdata/controller/config/SwaggerConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -16,8 +19,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-			.apis(RequestHandlerSelectors.basePackage("org.amalic.servicefromdata.controller"))
-			.build();
+				.apis(RequestHandlerSelectors.basePackage("org.amalic.servicefromdata.controller")).build();
 	}
 
 	@Override
@@ -25,5 +27,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
-	
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().docExpansion(DocExpansion.LIST).build();
+	}
+
 }
